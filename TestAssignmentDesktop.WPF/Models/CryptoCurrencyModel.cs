d72473a1b7c2bd1828488a21aef51ae1f356198f
Vso1o <vsolo1234@gmail.com>
2023-02-13 22:54:15 +0200

    Created WPF Project and displayed currencies with it

---
+++ TestAssignmentDesktop.WPF/Models/CryptoCurrencyModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestAssignmentDesktop.Core.Entities;
+
+namespace TestAssignmentDesktop.WPF.Models
+{
+    public class CryptoCurrencyModel
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double USDValue { get; set; }
+        public int Rank { get; set; }
+
+        public static CryptoCurrencyModel ConvertFromBase(BaseCryptoCurrencyInfoModel item)
+        {
+            return new CryptoCurrencyModel 
+            { 
+                Code =  item.CurrencyCode,
+                Name = item.CurrencyName, 
+                USDValue = item.USDValue,
+                Rank = item.Rank,
+            };
+        }
+    }
+}
---
+++ TestAssignmentDesktop.WPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TestAssignmentDesktop.Business.CrytoInfoReceiver;
+using TestAssignmentDesktop.WPF.Models;
+using TestAssignmentDesktop.WPF.ViewModels.Base;
+
+namespace TestAssignmentDesktop.WPF.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private ObservableCollection<CryptoCurrencyModel> _cryptoCurrencyModels = new ObservableCollection<CryptoCurrencyModel>();
+        public ObservableCollection<CryptoCurrencyModel> CryptoCurrencyModels
+        {
+            get { return _cryptoCurrencyModels; }
+            set
+            {
+                _cryptoCurrencyModels = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _shownCurrenciesCount = 10;
+        public int ShownCurrenciesCount
+        {
+            get { return _shownCurrenciesCount; }
+            set
+            {
+                _shownCurrenciesCount = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            Title= "CryptoCurrency Observer";
+            Task.Run(() => GetAssets());
+        }
+
+        private void GetAssets()
+        {
+            var receiver = new CoinCapReceiver();
+
+            var rawList = receiver.ReceiveAllAssets().Result;
+
+            ObservableCollection<CryptoCurrencyModel> result = new ObservableCollection<CryptoCurrencyModel>();
+
+            rawList.ForEach(asset => result.Add(CryptoCurrencyModel.ConvertFromBase(asset)));
+
+            //CryptoCurrencyModels = new ObservableCollection<CryptoCurrencyModel>();
+
+            /*foreach (var item in result)
+            {
+                CryptoCurrencyModels.Add(item);
+            }*/
+
+            CryptoCurrencyModels = result;
+        }
+    }
+}
